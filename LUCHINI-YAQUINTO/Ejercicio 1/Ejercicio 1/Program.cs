01df90fb47821bf91fa17d51ce830227eb35fc08
valenll <luchini.valu@gmail.com>
2025-05-27 15:48:55 -0300

    Semaforo resuelto

---
+++ LUCHINI-YAQUINTO/Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace LUCHINI_YAQUINTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaforo semaforo = new Semaforo("Rojo");
+
+            Console.WriteLine($"Estado inicial del semáforo: {semaforo.MostrarColor()}");
+
+            bool continuar = true;
+            int tiempo = 0;
+
+            Console.WriteLine("\nPresiona Ctrl+C para salir...\n");
+
+            while (continuar)
+            {
+                Thread.Sleep(1000);
+                tiempo++;
+
+                semaforo.PasoDelTiempo(1);
+                Console.WriteLine($"Segundo {tiempo}: {semaforo.MostrarColor()}");
+
+                if (tiempo == 10)
+                {
+                    Console.WriteLine("\n--- Activando intermitente ---");
+                    semaforo.PonerIntermitente();
+                }
+                else if (tiempo == 20)
+                {
+                    Console.WriteLine("\n--- Desactivando intermitente ---");
+                    semaforo.SacarIntermitente();
+                }
+            }
+        }
+    }
+}
