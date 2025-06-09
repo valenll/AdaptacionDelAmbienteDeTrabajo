0470541ffb3f8e15f9ecae506a39c732186dd860
LeonYaquinto <leonmailhou@gmail.com>
2025-06-09 14:43:20 -0300

    Ejercicio Cuenta Bancaria completado

---
+++ LUCHINI-YAQUINTO/Ejercicio 5/Ejercicio 5/Banco.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+public class Banco
+{
+    private Dictionary<string, CuentaBancaria> cuentas;
+
+    public Banco()
+    {
+        cuentas = new Dictionary<string, CuentaBancaria>();
+    }
+
+    public void AgregarCuenta(CuentaBancaria cuenta)
+    {
+        if (!cuentas.ContainsKey(cuenta.NumeroCuenta))
+        {
+            cuentas.Add(cuenta.NumeroCuenta, cuenta);
+        }
+    }
+
+    public bool Depositar(string numeroCuenta, decimal monto)
+    {
+        if (monto <= 0 || !cuentas.ContainsKey(numeroCuenta))
+            return false;
+
+        var cuenta = cuentas[numeroCuenta];
+        return cuenta.ModificarSaldo(monto);
+    }
+
+    public bool Extraer(string numeroCuenta, decimal monto)
+    {
+        if (monto <= 0 || !cuentas.ContainsKey(numeroCuenta))
+            return false;
+
+        var cuenta = cuentas[numeroCuenta];
+        return cuenta.ModificarSaldo(-monto);
+    }
+
+    public bool Transferencia(string cuentaOrigen, string cuentaDestino, decimal monto)
+    {
+        if (monto <= 0 ||
+            !cuentas.ContainsKey(cuentaOrigen) ||
+            !cuentas.ContainsKey(cuentaDestino))
+            return false;
+
+        var origen = cuentas[cuentaOrigen];
+        var destino = cuentas[cuentaDestino];
+
+        if (origen.ObtenerSaldo() < monto)
+            return false;
+
+        if (origen.ModificarSaldo(-monto))
+        {
+            destino.ModificarSaldo(monto);
+            return true;
+        }
+
+        return false;
+    }
+
+    public decimal ConsultarSaldo(string numeroCuenta)
+    {
+        if (cuentas.TryGetValue(numeroCuenta, out var cuenta))
+        {
+            return cuenta.ObtenerSaldo();
+        }
+        return -1;
+    }
+}
---
+++ LUCHINI-YAQUINTO/Ejercicio 5/Ejercicio 5/CuentaBancaria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class CuentaBancaria
+{
+    private string numeroCuenta;
+    private decimal saldo;
+    private string titular;
+
+    public CuentaBancaria(string numero, string titularCuenta, decimal saldoInicial = 0)
+    {
+        numeroCuenta = numero;
+        titular = titularCuenta;
+        saldo = saldoInicial;
+    }
+
+    public string NumeroCuenta => numeroCuenta;
+    public string Titular => titular;
+
+    public decimal ObtenerSaldo()
+    {
+        return saldo;
+    }
+
+    public bool ModificarSaldo(decimal monto)
+    {
+        if (saldo + monto < 0)
+            return false;
+
+        saldo += monto;
+        return true;
+    }
+}
